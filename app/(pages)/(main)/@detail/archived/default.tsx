70069d377b84500d06d85fa013fe8882e05d8faa
kisisellhesap <ahmetcannkaragoz@gmail.com>
2026-03-10 14:44:26 +0300

    updated

---
+++ app/(pages)/(main)/@detail/archived/default.tsx
@@ -0,0 +1,7 @@
+const Default = () => {
+  console.log(" @notes > notes > archived > page.tsx");
+
+  return null;
+};
+
+export default Default;
--- app/(pages)/(main)/@detail/archived/page.tsx
+++
@@ -1,9 +0,0 @@
-import NoteList from "@/app/components/slots/note-list";
-
-const Archived = () => {
-  console.log(" @notes > notes > archived > page.tsx");
-
-  return null;
-};
-
-export default Archived;
